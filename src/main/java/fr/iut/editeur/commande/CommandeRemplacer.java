e603f09fcc65ab6a614f5f92c527350cc4103898
Goetz_Hugo <hugo.goetz@etu.umontpellier.fr>
2023-11-07 13:52:18 +0100

    CommandeRemplacer 1er test

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,35 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if (parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+
+        try {
+            int depart = Integer.parseInt(parameters[1]);
+            int fin = Integer.parseInt(parameters[2]);
+            String chaine = parameters[3];
+            String texteDocument = document.getTexte();
+
+            if (depart < 0 || depart >= texteDocument.length() || fin <= depart || fin > texteDocument.length()) {
+                System.err.println("Indices de départ et de fin non valides.");
+                return;
+            }
+
+            document.remplacer(depart, fin, chaine);
+            super.executer();
+        } catch (NumberFormatException e) {
+            System.err.println("Les indices de départ et de fin doivent être des nombres entiers.");
+        }
+    }
+}
